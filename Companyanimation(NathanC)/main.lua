f74aa85fda2693cfb6b7cd8fbc2811e9dc7bf6e0
Nathan Cook <47784362+s266594@users.noreply.github.com>
2019-04-11 09:45:30 -0400

    Yes

--- Companyanimation(NathanC)/main.lua
+++ Companyanimation(NathanC)/main.lua
@@ -1,7 +1,18 @@
------------------------------------------------------------------------------------------
---
--- main.lua
---
------------------------------------------------------------------------------------------
+-- Title: WhackAMole
+-- Name: Your Name
+-- Course: ICS2O/3C
+-- This program displays my company logo on the screen spining
+-- with sound effects as well. 
+-------------------------------------
+--Hide status bar
+display.setStatusBar(display.HiddenStatusBar)
 
--- Your code here
+--set background colour
+display.setDefault("background", 0.4, 0.7, 0.5)
+
+local logo
+
+-- have image display
+logo = display.newImageRect("Images/CompanyLogoNathanC@2x.png", 300, 300)
+logo.x = 500
+logo.y = 500

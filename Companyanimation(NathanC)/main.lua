932a75f1dccbaa0b59e910288606d05372470b7e
Nathan Cook <47784362+s266594@users.noreply.github.com>
2019-04-12 09:17:32 -0400

    done

--- Companyanimation(NathanC)/main.lua
+++ Companyanimation(NathanC)/main.lua
@@ -10,9 +10,53 @@ display.setStatusBar(display.HiddenStatusBar)
 --set background colour
 display.setDefault("background", 0.4, 0.7, 0.5)
 
+-----------------------------------------------------------------------------------
+--LOCAL VARIABLES
+----------------------------------------------------------------------------------
 local logo
+local scrollXSpeed = 7
+local scrollYSpeed = -3
+local soundef = audio.loadSound("Sounds/sound8.mp3")
+local soundefChannel
+local scrollXSpeed2 = -10
+local scrollYSpeed2 = -12
+-----------------------------------------------------------------------------------------
 
 -- have image display
 logo = display.newImageRect("Images/CompanyLogoNathanC@2x.png", 300, 300)
-logo.x = 500
-logo.y = 500
+logo.x = 100
+logo.y = 700
+logo2 = display.newImageRect("Images/CompanyLogoNathanC@2x.png", 300, 300)
+logo2.x = 500
+logo2.y = 500
+logo3 = display.newImageRect("Images/CompanyLogoNathanC@2x.png", 300, 300)
+logo3.x = 1030
+logo3.y = 700
+------------------------------------------------------------------------------------------
+-- LOCAL FUNCTIONS
+--------------------------------------------------------------------------------
+local function moveLogo()
+	logo.x = logo.x + scrollXSpeed
+	logo.y = logo.y + scrollYSpeed
+	-- change transparencey of logo
+	logo.alpha = logo.alpha - 0.004
+	-- rotate logo
+	logo.rotation = logo.rotation + 4
+	-- Play audio
+	soundefChannel = audio.play(soundef)
+end
+
+local function moveLogo2()
+	logo3.x = logo3.x + scrollXSpeed2
+	logo3.y = logo3.y + scrollYSpeed2
+	-- change transparencey of logo
+	logo3.alpha = logo3.alpha - 0.004
+	-- rotate logo
+	logo3.rotation = logo3.rotation + 4
+	-- Play audio
+	soundefChannel = audio.play(soundef)
+end
+
+Runtime:addEventListener("enterFrame", moveLogo)
+Runtime:addEventListener("enterFrame", moveLogo2)
+
